affd799494608243d0d605f64d0e4d6c0703e236
dediacc <dedisuhaimiacc@gmail.com>
2019-06-18 20:12:02 +0700

    add more comment

--- counter.py
+++ counter.py
@@ -1,8 +1,16 @@
 import numpy as np
 import scipy.sparse as scs
 
+
 def make_matrix(num_of_rows, num_of_cols):
-    # make adjacency matrix
+    '''
+    this function construct and return the adjacency matrix of a grid graph
+    params:
+        num_of_rows : how many rows does the grid graph has
+        num_of_cols : how many columns does the grid graph
+        basically, what is the dimension of the grid graph 
+
+    '''
     n = num_of_rows*num_of_cols
     M = np.zeros((n, n))
     for r in range(0, num_of_rows):
@@ -14,12 +22,21 @@ def make_matrix(num_of_rows, num_of_cols):
                 M[i-num_of_cols, i] = M[i, i-num_of_cols] = 1
     return M
 
+
 def calc_num_spanning(num_of_rows, num_of_cols):
+    '''
+    this function calculates the number of spanning tree using matrix tree theorem
+    params :
+        num_of_rows : how many rows does the grid graph has
+        num_of_cols : how many columns does the grid graph
+        basically, what is the dimension of the grid graph        
+    '''
     A = make_matrix(num_of_rows, num_of_cols)
-    
+
     # calculate any cofactor of its Laplacian graph (see Matrix Tree theorem)
     new_L = scs.csgraph.laplacian(A)[1:, 1:]
     result = int(np.round(np.linalg.det(new_L)))
     return result
 
-print(calc_num_spanning(4,4))
+
+print(calc_num_spanning(4, 4))
